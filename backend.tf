7707958f7d99d9ea31bca6ac21f79be50cbdcc1d
Krishna <krishnads290#gmail.com>
2026-03-29 16:29:24 +0530

    new repo

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-aws-tf-state-bucket"
+    bucket         = "terraform-aws-tf-state-bucket-1"
     key            = "terraform.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "aws-tf-lock-table"
